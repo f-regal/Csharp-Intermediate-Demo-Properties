b5f3580998f4249e483c9c9757ba6e89ad3de9e7
Faysal Regal <faysalregal@gmail.com>
2022-10-02 20:15:21 +0100

    add birthdate property

--- Csharp-Intermediate-Demo-Properties/Program.cs
+++ Csharp-Intermediate-Demo-Properties/Program.cs
@@ -2,6 +2,13 @@
 
 namespace Csharp_Intermediate_Demo_Properties
 {
+
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
