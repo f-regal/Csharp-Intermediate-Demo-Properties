56cb3b2650c6f5193c7ce839753cee72e1ea2d97
Faysal Regal <faysalregal@gmail.com>
2022-10-02 19:37:58 +0100

    first commit c# properties

---
+++ Csharp-Intermediate-Demo-Properties/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Intermediate_Demo_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
